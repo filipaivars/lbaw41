423fc407df85abfb529f8a5be207d6f6b27c2333
Luís Miguel Gonçalves <g.luismiguel14@gmail.com>
2016-05-23 18:04:06 +0100

    fixes

--- pages/users/users.php
+++ pages/users/users.php
@@ -11,9 +11,9 @@ if (!$_GET['user_id']) {
 $user_id = $_GET['user_id'];
 $user = getUserInfo($user_id)[0];
 $user_questions = getUserLastQuestions($user_id);
-$user["favourites"] = getUserFavourites($user_id);
-$user["questions"] = getUserQuestions($user_id);
-$user["answers"] = getUserAnswers($user_id);
+$user["favourites"] = getUserFavourites($user_id)[0];
+$user["questions"] = getUserQuestions($user_id)[0];
+$user["answers"] = getUserAnswers($user_id)[0];
 
 $smarty->assign('user', $user);
 $smarty->display('users/users.tpl');
