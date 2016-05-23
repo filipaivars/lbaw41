d4f67886f012130c805ac36bdd53432cb112dce7
Luís Miguel Gonçalves <g.luismiguel14@gmail.com>
2016-05-23 15:57:42 +0100

    Question querry fixed

--- database/perguntas.php
+++ database/perguntas.php
@@ -3,7 +3,7 @@
 function getLastPerguntas() {
     global $conn;
     $stmt = $conn->prepare("
-        SELECT pergunta.pergunta_id,pergunta.titulo,pergunta.created_date,utilizador.username, count(DISTINCT resposta.resposta_id) as n_respostas, avg(votoutilizadorpergunta.valor) as average
+        SELECT pergunta.pergunta_id,pergunta.titulo,pergunta.created_date,utilizador.username, pergunta.criar_id, count(DISTINCT resposta.resposta_id) as n_respostas, avg(votoutilizadorpergunta.valor) as average
         FROM pergunta 
         JOIN utilizador ON (pergunta.criar_id = utilizador.user_id) 
         LEFT OUTER JOIN resposta on (pergunta.pergunta_id = resposta.pergunta_id) 
@@ -20,7 +20,7 @@ function getLastPerguntas() {
 function getBestPerguntas() {
     global $conn;
     $stmt = $conn->prepare("
-        SELECT pergunta.pergunta_id,pergunta.titulo,pergunta.created_date,utilizador.username, count(DISTINCT resposta.resposta_id) as n_respostas, avg(votoutilizadorpergunta.valor) as average
+        SELECT pergunta.pergunta_id,pergunta.titulo,pergunta.created_date,utilizador.username, pergunta.criar_id, count(DISTINCT resposta.resposta_id) as n_respostas, avg(votoutilizadorpergunta.valor) as average
         FROM pergunta 
         JOIN utilizador ON (pergunta.criar_id = utilizador.user_id) 
         LEFT OUTER JOIN resposta on (pergunta.pergunta_id = resposta.pergunta_id) 
