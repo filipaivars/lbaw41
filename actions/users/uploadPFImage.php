ad53c3d8fce783b08b7804611cf8dcd14b502172
DrAntarctica <andrefpinto5@gmail.com>
2016-06-04 20:34:47 +0100

    test

--- actions/users/uploadPFImage.php
+++ actions/users/uploadPFImage.php
@@ -6,6 +6,7 @@
 	$target_file = $BASE_DIR . "images/users/" . basename($_FILES["fileToUpload"]["name"]);
 	$db_dir = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 	$uploadOk = 1;
+	$changeOk = 1;
 	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 	if(isset($_POST["submit"])) {
@@ -16,6 +17,7 @@
 		} else {
 			echo "File is not an image.";
 			$uploadOk = 0;
+			$changeOk = 0;
 		}
 	}
 // Check if file already exists
@@ -27,12 +29,14 @@
 	if ($_FILES["fileToUpload"]["size"] > 500000) {
 		echo "Sorry, your file is too large.";
 		$uploadOk = 0;
+		$changeOk = 0;
 	}
 // Allow certain file formats
 	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 	&& $imageFileType != "gif" ) {
 		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
 		$uploadOk = 0;
+		$changeOk = 0;
 	}
 // Check if $uploadOk is set to 0 by an error
 	if ($uploadOk == 0) {
@@ -44,6 +48,10 @@
 			changeAvatar($user_id,$db_dir);
 			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 		} else {
+			if($changeOk == 1) {
+				$user_id = getUserId($_SESSION['username'])['user_id'];
+				changeAvatar($user_id,$db_dir);
+			}
 			echo "Sorry, there was an error uploading your file.";
     }
 }
