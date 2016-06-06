7d5c92e34dfe783e2baee657b488f56e10a14cb7
Alexandre Lima <infus@sapo.pt>
2016-06-06 03:45:46 +0100

    vote stuff

--- api/votes/votePergunta.php
+++ api/votes/votePergunta.php
@@ -2,7 +2,7 @@
 include_once('../../config/init.php');
 include_once($BASE_DIR .'database/perguntas.php');
 
-getTweetCountAfter($_GET['id']);
+
 
 if(existsVotoPergunta($_GET['user_id'], $_GET['pergunta_id'])) {
     updateVotoPergunta($_GET['user_id'], $_GET['pergunta_id'],$_GET['valor']);
