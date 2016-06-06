b37d71cd6a5fc70a14230c0e71b6a1fba7164155
Alexandre Lima <infus@sapo.pt>
2016-06-06 02:09:01 +0100

    vote stuff

--- javascript/perguntas.js
+++ javascript/perguntas.js
@@ -1,4 +1,6 @@
-function votoPergunta(userid,perguntaid,valor) {
+BASE_URL = "...";
+
+    function votoPergunta(userid,perguntaid,valor) {
     console.log("HEEEEEEEEEELOOOOOOOOOOOOOOO");
     $.getJSON(BASE_URL + "api/votes/votePergunta.php", {user_id: userid,pergunta_id: perguntaid, valor: valor}, null);
 
