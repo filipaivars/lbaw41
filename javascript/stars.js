aaf820d0d7f5fa00e9d6eba25032b10e0fcd96f6
Alexandre Lima <infus@sapo.pt>
2016-06-06 03:45:04 +0100

    vote stuff

--- javascript/stars.js
+++ javascript/stars.js
@@ -139,7 +139,7 @@
 
 
 function votoPergunta(userid,perguntaid,valor) {
-    console.log("HEEEEEEEEEELOOOOOOOOOOOOOOO");
+    console.log(userid + " " + perguntaid + " " + valor);
     $.getJSON(BASE_URL + "api/votes/votePergunta.php", {user_id: userid,pergunta_id: perguntaid, valor: valor}, null);
 
 }
